d0583a92c8cff568f33c32cb0b1711f7ed4a4f4d
nichprice <price.nich@gmail.com>
2022-06-01 10:57:16 -0400

    html set up for form and index.js set up w click and submit

--- index.js
+++ index.js
@@ -5,10 +5,32 @@ document.addEventListener('DOMContentLoaded', () =>
 })
 
 const brewList = document.getElementById('breweries-list')
+const brewName = document.getElementById('brewery-name')
+const brewCity = document.getElementById('brewery-city')
+const brewState = document.getElementById('brewery-state')
+const newBrewForm = document.getElementById('new-brewery')
+const newBrewName = document.getElementById('new-name')
+
+
+const brewPic = document.getElementById('image')
 
 function renderBreweries(breweries) {
   breweries.forEach(brewery => {
-    let addSpan = document.createElement('span')
-    addSpan.innerText = brewery.name
+    let brewSpan = document.createElement('span')
+    brewSpan.innerText = brewery.name
+
+    brewSpan.addEventListener('click', (e) => {
+        brewName.innerText = brewery.name
+        brewCity.innerText = brewery.city
+        brewState.innerText = brewery.state
+      })
   });
+  
+  newBrewForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    newBrewSpan = document.createElement('span')
+    newBrewSpan.innerText = newBrewName.value
+
+    brewList.append(newBrewSpan)
+  })
 }
